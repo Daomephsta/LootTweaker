e47903f25992cf39ee48d31eb1cb902220cc35f7
Daomephsta <dtnilfisk@gmail.com>
2017-02-25 15:06:41 +1300

    Add /mt loottables list

--- src/main/java/leviathan143/loottweaker/common/commands/CommandLootTables.java
+++ src/main/java/leviathan143/loottweaker/common/commands/CommandLootTables.java
@@ -12,64 +12,86 @@ import net.minecraft.world.storage.loot.LootTableList;
 
 public class CommandLootTables implements ICommandFunction
 {
-	public static final String NAME = "loottables";
-	public static final String[] DESCRIPTION = new String[]{"Dumps the specified loottable(s) to <minecraft folder>/dumps"};
+    public static final String NAME = "loottables";
+    public static final String[] DESCRIPTION = new String[]{"Dumps the specified loottable(s) to <minecraft folder>/dumps"};
 
-	@Override
-	public void execute(String[] args, IPlayer player) 
+    private static enum Option
+    {
+	all,
+	entity,
+	byName,
+	list;
+    }
+
+    @Override
+    public void execute(String[] args, IPlayer player) 
+    {
+	if(args.length < 1)
 	{
-		if(args.length < 1)
-		{
-			player.sendChat("Usage: /mt loottables [mode] <args>");
-			player.sendChat("Modes:| all|  entity  | byName  |");
-			player.sendChat("Args: |none|entityName|tableName|");
-			return;
-		}
-		if(args[0].equals("all"))
-		{
-			for(ResourceLocation tableLoc : LootTableList.getAll())
-			{
-				LootUtils.writeTableToJSON(tableLoc, LootTweakerMain.proxy.getWorld().getLootTableManager(), getLootTableDumpFilePath(tableLoc), true);
-			}
-			player.sendChat("Done!");
-		}
-		else if(args[0].equals("entity"))
-		{
-			if(args.length < 2)
-			{
-				player.sendChat("Entity name required!");
-				return;
-			}
-			if(!EntityList.NAME_TO_CLASS.containsKey(args[1]))
-			{
-				player.sendChat("Invalid entity name!");
-				return;
-			}
-			ResourceLocation tableLoc = LootUtils.getEntityLootTableFromName(args[1]);
-			if(tableLoc == null) return;
-			LootUtils.writeTableToJSON(tableLoc, LootTweakerMain.proxy.getWorld().getLootTableManager(), getLootTableDumpFilePath(tableLoc), true);
-			player.sendChat("Done!");
-		}
-		else if(args[0].equals("byName"))
-		{
-			if(args.length < 2)
-			{
-				player.sendChat("Loot table name required!");
-				return;
-			}
-			ResourceLocation tableLoc = new ResourceLocation(args[1]);
-			if(!LootTableList.getAll().contains(tableLoc))
-			{
-				player.sendChat("Invalid loot table name!");
-				return;
-			}
-			LootUtils.writeTableToJSON(tableLoc, LootTweakerMain.proxy.getWorld().getLootTableManager(), getLootTableDumpFilePath(tableLoc), true);
-			player.sendChat("Done!");
-		}
+	    player.sendChat("Usage: /mt loottables [mode] <args>");
+	    player.sendChat("Modes:| all|  entity  | byName  |");
+	    player.sendChat("Args: |none|entityName|tableName|");
+	    return;
 	}
-	
-	private static File getLootTableDumpFilePath(ResourceLocation tableLoc)
+	Option option = Enum.valueOf(Option.class, args[0]);
+	ResourceLocation tableLoc = null;
+	switch (option)
 	{
-		return new File("dumps" + File.separator + "loot_tables" + File.separator + tableLoc.getResourceDomain() + File.separator + tableLoc.getResourcePath() + ".json");
+	case all:
+	    for(ResourceLocation table : LootTableList.getAll())
+	    {
+		LootUtils.writeTableToJSON(tableLoc, LootTweakerMain.proxy.getWorld().getLootTableManager(), getLootTableDumpFilePath(table), true);
+	    }
+	    player.sendChat("Done!");
+	    break;
+	    
+	case byName:
+	    if(args.length < 2)
+	    {
+		player.sendChat("Loot table name required!");
+		return;
+	    }
+	    tableLoc = new ResourceLocation(args[1]);
+	    if(!LootTableList.getAll().contains(tableLoc))
+	    {
+		player.sendChat("Invalid loot table name!");
+		return;
+	    }
+	    LootUtils.writeTableToJSON(tableLoc, LootTweakerMain.proxy.getWorld().getLootTableManager(), getLootTableDumpFilePath(tableLoc), true);
+	    player.sendChat("Done!");
+	    break;
+	    
+	case entity:
+	    if(args.length < 2)
+	    {
+		player.sendChat("Entity name required!");
+		return;
+	    }
+	    if(!EntityList.NAME_TO_CLASS.containsKey(args[1]))
+	    {
+		player.sendChat("Invalid entity name!");
+		return;
+	    }
+	    tableLoc = LootUtils.getEntityLootTableFromName(args[1]);
+	    if(tableLoc == null) return;
+	    LootUtils.writeTableToJSON(tableLoc, LootTweakerMain.proxy.getWorld().getLootTableManager(), getLootTableDumpFilePath(tableLoc), true);
+	    player.sendChat("Done!");
+	    break;
+	    
+	case list:
+	    for(ResourceLocation table : LootTableList.getAll())
+	    {
+		player.sendChat(table.toString());
+	    }
+	    break;
+	    
+	default:
+	    break;
 	}
+    }
+
+    private static File getLootTableDumpFilePath(ResourceLocation tableLoc)
+    {
+	return new File("dumps" + File.separator + "loot_tables" + File.separator + tableLoc.getResourceDomain() + File.separator + tableLoc.getResourcePath() + ".json");
+    }
 }
