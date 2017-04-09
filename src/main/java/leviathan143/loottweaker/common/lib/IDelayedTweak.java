f007117672d2d89949e38ef7f4b692683ffaaeaa
Daomephsta <dtnilfisk@gmail.com>
2017-04-09 21:50:45 +1200

    Rework pool tweaking system

---
+++ src/main/java/leviathan143/loottweaker/common/lib/IDelayedTweak.java
@@ -0,0 +1,6 @@
+package leviathan143.loottweaker.common.lib;
+
+public interface IDelayedTweak<LootObject, ZenWrapper>
+{
+    public void applyTweak(LootObject lootObject, ZenWrapper zenWrapper);
+}
