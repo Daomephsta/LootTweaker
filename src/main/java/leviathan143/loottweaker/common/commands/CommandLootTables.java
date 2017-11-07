9e3ece834eebec4d8ed6c80cffc07bdbb87a91cd
Leviathan143 <Leviathan143@users.noreply.github.com>
2017-11-07 16:55:38 +1300

    Replace \ct loottables entity subcommand with \ct loottables target, which gets the loot table of the targeted entity.

--- src/main/java/leviathan143/loottweaker/common/commands/CommandLootTables.java
+++ src/main/java/leviathan143/loottweaker/common/commands/CommandLootTables.java
@@ -1,15 +1,31 @@
 package leviathan143.loottweaker.common.commands;
 
 import java.io.File;
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
 
 import crafttweaker.mc1120.commands.CraftTweakerCommand;
 import leviathan143.loottweaker.common.LootTweakerMain;
+import leviathan143.loottweaker.common.darkmagic.CommonMethodHandles;
 import leviathan143.loottweaker.common.lib.LootUtils;
 import net.minecraft.command.ICommandSender;
-import net.minecraft.entity.EntityList;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EntitySelectors;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.text.TextComponentString;
+import net.minecraft.world.storage.loot.ILootContainer;
 import net.minecraft.world.storage.loot.LootTableList;
 
 public class CommandLootTables extends CraftTweakerCommand
@@ -18,17 +34,15 @@ public class CommandLootTables extends CraftTweakerCommand
     {
 	super("loottables");
     }
-    
-    public static final String[] DESCRIPTION = new String[]{"/mt loottables [mode] <args>", "    Dumps the specified loottable(s)", "    to <minecraft folder>/dumps"};
 
     private static enum Option
     {
 	all,
-	entity,
+	target,
 	byName,
 	list;
     }
-    
+
     @Override
     protected void init()
     {
@@ -56,7 +70,7 @@ public class CommandLootTables extends CraftTweakerCommand
 	    }
 	    sender.sendMessage(new TextComponentString("Done!"));
 	    break;
-	    
+
 	case byName:
 	    if(args.length < 2)
 	    {
@@ -72,32 +86,42 @@ public class CommandLootTables extends CraftTweakerCommand
 	    LootUtils.writeTableToJSON(tableLoc, LootTweakerMain.proxy.getWorld().getLootTableManager(), getLootTableDumpFilePath(tableLoc), true);
 	    sender.sendMessage(new TextComponentString("Done!"));
 	    break;
-	    
-	case entity:
-	    if(args.length < 2)
+
+	case target:
+	    if(sender instanceof Entity)
 	    {
-		sender.sendMessage(new TextComponentString("Entity name required!"));
-		return;
+		RayTraceResult target = getLookTarget((Entity) sender, 8.0D);
+		switch(target.typeOfHit)
+		{
+		case BLOCK:
+		    TileEntity te = sender.getEntityWorld().getTileEntity(target.getBlockPos());
+		    if(te instanceof ILootContainer) tableLoc = ((ILootContainer) te).getLootTable();
+		    else sender.sendMessage(new TextComponentString("The target does not have a loot table"));
+		    break;
+		case ENTITY:
+		    if(target.entityHit instanceof EntityLiving) tableLoc = CommonMethodHandles.getEntityLootTable((EntityLiving) target.entityHit);
+		    else sender.sendMessage(new TextComponentString("The target does not have a loot table"));
+		    break;
+		case MISS: 
+		    sender.sendMessage(new TextComponentString("Nothing in range")); 
+		    return;
+		default: 
+		    return;
+		}
+		if(tableLoc == null) return;
+		LootUtils.writeTableToJSON(tableLoc, LootTweakerMain.proxy.getWorld().getLootTableManager(), getLootTableDumpFilePath(tableLoc), true);
+		sender.sendMessage(new TextComponentString("Done!"));
 	    }
-	    ResourceLocation entityName = new ResourceLocation(args[1]);
-	    if(!EntityList.isRegistered(entityName))
-	    {
-		sender.sendMessage(new TextComponentString("Invalid entity name!"));
-		return;
-	    }
-	    tableLoc = LootUtils.getEntityLootTableFromName(entityName);
-	    if(tableLoc == null) return;
-	    LootUtils.writeTableToJSON(tableLoc, LootTweakerMain.proxy.getWorld().getLootTableManager(), getLootTableDumpFilePath(tableLoc), true);
-	    sender.sendMessage(new TextComponentString("Done!"));
+	    else sender.sendMessage(new TextComponentString("This command can only be executed by entities"));
 	    break;
-	    
+
 	case list:
 	    for(ResourceLocation table : LootTableList.getAll())
 	    {
 		sender.sendMessage(new TextComponentString(table.toString()));
 	    }
 	    break;
-	    
+
 	default:
 	    break;
 	}
@@ -106,5 +130,81 @@ public class CommandLootTables extends CraftTweakerCommand
     private static File getLootTableDumpFilePath(ResourceLocation tableLoc)
     {
 	return new File("dumps" + File.separator + "loot_tables" + File.separator + tableLoc.getResourceDomain() + File.separator + tableLoc.getResourcePath() + ".json");
+    }
+
+    //Copied from EntityRenderer#getMouseOver() and modified to work on any side
+    private static RayTraceResult getLookTarget(Entity entity, double distance)
+    {
+	RayTraceResult result = null;
+	Entity targetedEntity = null;
+
+	Vec3d eyePos = entity.getPositionEyes(1.0F);
+	Vec3d look = entity.getLook(1.0F);
+	Vec3d lookTarget = eyePos.addVector(look.x * distance, look.y * distance, look.z * distance);
+	result = entity.getEntityWorld().rayTraceBlocks(eyePos, lookTarget, false, false, true);
+	boolean flag = false;
+	double d1 = distance;
+
+	if (result != null)
+	{
+	    d1 = result.hitVec.distanceTo(eyePos);
+	}
+
+	Vec3d vec3d3 = null;
+	List<Entity> list = entity.getEntityWorld().getEntitiesInAABBexcluding(entity, entity.getEntityBoundingBox().expand(look.x * distance, look.y * distance, look.z * distance).grow(1.0D, 1.0D, 1.0D), Predicates.and(EntitySelectors.NOT_SPECTATING, new Predicate<Entity>()
+	{
+	    public boolean apply(@Nullable Entity p_apply_1_)
+	    {
+		return p_apply_1_ != null && p_apply_1_.canBeCollidedWith();
+	    }
+	}));
+	double d2 = d1;
+
+	for (Entity entity1 : list)
+	{
+	    AxisAlignedBB axisalignedbb = entity1.getEntityBoundingBox().grow((double)entity1.getCollisionBorderSize());
+	    RayTraceResult raytraceresult = axisalignedbb.calculateIntercept(eyePos, lookTarget);
+
+	    if (axisalignedbb.contains(eyePos))
+	    {
+		if (d2 >= 0.0D)
+		{
+		    targetedEntity = entity1;
+		    vec3d3 = raytraceresult == null ? eyePos : raytraceresult.hitVec;
+		    d2 = 0.0D;
+		}
+	    }
+	    else if (raytraceresult != null)
+	    {
+		double d3 = eyePos.distanceTo(raytraceresult.hitVec);
+
+		if (d3 < d2 || d2 == 0.0D)
+		{
+		    if (entity1.getLowestRidingEntity() == entity.getLowestRidingEntity() && !entity1.canRiderInteract())
+		    {
+			if (d2 == 0.0D)
+			{
+			    targetedEntity = entity1;
+			    vec3d3 = raytraceresult.hitVec;
+			}
+		    }
+		    else
+		    {
+			targetedEntity = entity1;
+			vec3d3 = raytraceresult.hitVec;
+			d2 = d3;
+		    }
+		}
+	    }
+	}
+
+	if (targetedEntity != null && flag && eyePos.distanceTo(vec3d3) > 3.0D)
+	{
+	    targetedEntity = null;
+	    result = new RayTraceResult(RayTraceResult.Type.MISS, vec3d3, (EnumFacing)null, new BlockPos(vec3d3));
+	}
+
+	if (targetedEntity != null && (d2 < d1 || result == null)) result = new RayTraceResult(targetedEntity, vec3d3);
+	return result;
     }
 }
