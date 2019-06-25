70930064ee7de0f5d0444308bf228c0de291738c
Daomephsta <dtnilfisk@gmail.com>
2019-06-25 18:52:10 +1200

    Remove ModConditionHelper & ModFunctionHelper, in favour of having mods or an LT addon implement support via their own ZenClass.

--- src/main/java/leviathan143/loottweaker/common/CommonProxy.java
+++ src/main/java/leviathan143/loottweaker/common/CommonProxy.java
@@ -1,20 +1,18 @@
 package leviathan143.loottweaker.common;
 
-import crafttweaker.CraftTweakerAPI;
 import crafttweaker.mc1120.commands.CTChatCommand;
 import leviathan143.loottweaker.common.commands.CommandLootTables;
-import leviathan143.loottweaker.common.zenscript.ModConditionHelper;
-import leviathan143.loottweaker.common.zenscript.ModFunctionHelper;
 import net.minecraft.world.World;
-import net.minecraftforge.fml.common.event.*;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLServerAboutToStartEvent;
+import net.minecraftforge.fml.common.event.FMLServerStartedEvent;
 
 public abstract class CommonProxy
 {
 	public void preInit(FMLPreInitializationEvent event)
 	{
-		//Register global symbols
-		CraftTweakerAPI.registerGlobalSymbol("modConditions", CraftTweakerAPI.getJavaStaticFieldSymbol(ModConditionHelper.class, "INSTANCE"));
-		CraftTweakerAPI.registerGlobalSymbol("modFunctions", CraftTweakerAPI.getJavaStaticFieldSymbol(ModFunctionHelper.class, "INSTANCE"));
 		LTConfig.onLoad();
 	}
 
