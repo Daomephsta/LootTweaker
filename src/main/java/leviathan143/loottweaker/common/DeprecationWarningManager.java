8cfa2b81fe187285e409841d7f9285f1dc5c2139
Daomephsta <dtnilfisk@gmail.com>
2019-06-25 19:15:59 +1200

    Remove DeprecationWarningManager.fixChatSpam, since CraftTweaker has fixed the issue. Closes #48.

--- src/main/java/leviathan143/loottweaker/common/DeprecationWarningManager.java
+++ src/main/java/leviathan143/loottweaker/common/DeprecationWarningManager.java
@@ -4,9 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import crafttweaker.CraftTweakerAPI;
-import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import stanhebben.zenscript.annotations.ZenClass;
 
 /**
@@ -38,24 +36,11 @@ public class DeprecationWarningManager
 	public static void printDeprecationWarnings()
 	{
 		if(deprecatedObjectsUsed.isEmpty() || !LTConfig.deprecationWarnings) return;
-		warnings = 0;
 		CraftTweakerAPI.logWarning("LootTweaker: One or more scripts use deprecated methods. Check crafttweaker.log for more information.");
 		for(String deprecatedObj : deprecatedObjectsUsed)
 		{
 			CraftTweakerAPI.logInfo(String.format("%s is deprecated", deprecatedObj));
 		}
 		CraftTweakerAPI.logInfo("Refer to https://github.com/Leviathan143/LootTweaker/wiki/Deprecations for more info. This warning can be disabled in the config.");
-	}
-
-	//Why you make me do this, CraftTweaker? :..(
-	private static int warnings = 0;
-	@SubscribeEvent
-	public static void fixChatSpam(ClientChatReceivedEvent e)
-	{
-		if(e.getMessage().getFormattedText().contains("LootTweaker"))
-		{
-			warnings++;
-			if(warnings > 1) e.setCanceled(true);
-		}
 	}
 }
