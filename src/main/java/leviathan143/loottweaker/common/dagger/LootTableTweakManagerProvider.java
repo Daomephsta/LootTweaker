861e92d9036a65669ac2639b0b72e0fc14b1e3db
Daomephsta <dtnilfisk@gmail.com>
2019-11-01 11:39:47 +1300

    Correct incorrect module

--- src/main/java/leviathan143/loottweaker/common/dagger/LootTableTweakManagerProvider.java
+++ src/main/java/leviathan143/loottweaker/common/dagger/LootTableTweakManagerProvider.java
@@ -3,11 +3,10 @@ package leviathan143.loottweaker.common.dagger;
 import javax.inject.Singleton;
 
 import dagger.Component;
-import io.github.daomephsta.loottweaker.test.dagger.TestModule;
 import leviathan143.loottweaker.common.zenscript.LootTableTweakManager;
 
 @Singleton
-@Component(modules = TestModule.class)
+@Component(modules = ProductionModule.class)
 public interface LootTableTweakManagerProvider
 {
     LootTableTweakManager provide();
