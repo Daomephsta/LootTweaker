031979645150fc937bfa9d606600d16f03f681f9
Daomephsta <dtnilfisk@gmail.com>
2020-04-13 18:02:02 +1200

    Initial implementation of table creation

--- src/main/java/leviathan143/loottweaker/common/zenscript/LootTableTweakManager.java
+++ src/main/java/leviathan143/loottweaker/common/zenscript/LootTableTweakManager.java
@@ -1,21 +1,25 @@
 package leviathan143.loottweaker.common.zenscript;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.File;
+import java.util.*;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import leviathan143.loottweaker.common.lib.LootTableDumper;
 import leviathan143.loottweaker.common.mutable_loot.MutableLootTable;
 import leviathan143.loottweaker.common.zenscript.wrapper.ZenLootTableWrapper;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
 import net.minecraft.world.storage.loot.LootTable;
 
 public class LootTableTweakManager
 {
     private static final Logger LOGGER = LogManager.getLogger();
     private final Map<ResourceLocation, ZenLootTableWrapper> tweakedTables = new HashMap<>();
+    private final Collection<ZenLootTableWrapper> tableBuilders = new HashSet<>();
     private final LootTweakerContext context;
+    private File lastWorldDirectory;
 
     LootTableTweakManager(LootTweakerContext context)
     {
@@ -45,6 +49,28 @@ public class LootTableTweakManager
                 tweakedTables.put(tableId, wrapper);
         }
         return wrapper;
+    }
+
+    public ZenLootTableWrapper newTable(String id)
+    {
+        ZenLootTableWrapper builder = context.wrapLootTable(new ResourceLocation(id));
+        tableBuilders.add(builder);
+        return builder;
+    }
+
+    public void onWorldLoad(World world)
+    {
+        //Only dump once per save
+        if (Objects.equals(lastWorldDirectory, world.getSaveHandler().getWorldDirectory()))
+            return;
+        LootTableDumper dumper = new LootTableDumper(new File(world.getSaveHandler().getWorldDirectory(), "data/loot_tables"));
+        for (ZenLootTableWrapper builder : tableBuilders)
+        {
+            MutableLootTable mutableTable = new MutableLootTable(builder.getId(), new HashMap<>());
+            builder.applyTweakers(mutableTable);
+            dumper.dump(mutableTable.toImmutable(), builder.getId());
+        }
+        lastWorldDirectory = world.getSaveHandler().getWorldDirectory();
     }
 
     public LootTable tweakTable(ResourceLocation tableId, LootTable table)
