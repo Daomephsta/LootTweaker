574f19e91ba8bb850b06502f07cfbd6c9dc39234
Daomephsta <dtnilfisk@gmail.com>
2020-07-18 12:45:33 +1200

    Remove erroneous @IterableSimple annotation from LootTableRepresentation

--- src/main/java/leviathan143/loottweaker/common/zenscript/api/LootTableRepresentation.java
+++ src/main/java/leviathan143/loottweaker/common/zenscript/api/LootTableRepresentation.java
@@ -2,13 +2,11 @@ package leviathan143.loottweaker.common.zenscript.api;
 
 import crafttweaker.annotations.ZenRegister;
 import leviathan143.loottweaker.common.LootTweaker;
-import stanhebben.zenscript.annotations.IterableSimple;
 import stanhebben.zenscript.annotations.ZenClass;
 import stanhebben.zenscript.annotations.ZenMethod;
 
 @ZenRegister
 @ZenClass(LootTweaker.MODID + ".LootTable")
-@IterableSimple(LootTweaker.MODID + ".RemovableLootPool")
 public interface LootTableRepresentation
 {
     @ZenMethod
